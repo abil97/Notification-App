30441ae4942fee002c5acf4c7be88b1b611577af
abil <abil.kuatbayev@nu.edu.kz>
2020-02-12 16:32:03 +0600

    Added Sound, Vibration & Tap Action

--- app/src/main/java/com/example/notifications/App.java
+++ app/src/main/java/com/example/notifications/App.java
@@ -3,6 +3,8 @@ package com.example.notifications;
 import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.media.AudioAttributes;
+import android.net.Uri;
 import android.os.Build;
 
 public class App extends Application {
@@ -27,7 +29,18 @@ public class App extends Application {
                     "Main Channel",
                     NotificationManager.IMPORTANCE_DEFAULT
             );
+
+            Uri uri = Uri.parse("android.resource://"+ this.getPackageName()+"/" + R.raw.ios_notification);
+
+            AudioAttributes att = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                    .build();
+
+            channel.enableVibration(true);
             channel.setDescription("Main channel for the Notification");
+            channel.setVibrationPattern(new long[] {0, 500, 500, 500});
+            channel.setSound(uri, att);
 
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.notifications;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.AudioAttributes;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -23,26 +26,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        notifiactionManager = NotificationManagerCompat.from(this);
+        //notifiactionManager = NotificationManagerCompat.from(this);
         title = findViewById(R.id.editText);
         message = findViewById(R.id.editText2);
 
     }
 
     public void sendNotification(View v){
-        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_mood_black_24dp)
-                .setContentTitle(title.getText().toString())
-                .setContentText(message.getText().toString())
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setVibrate(new long[] {1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000})
-                .setSound(Uri.parse("android.resource://"
-                        + this.getPackageName() + "/" + R.raw.ios_notification))
-                .build();
+
+        Intent intent = new Intent(this, App.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this, App.CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_mood_black_24dp)
+                        .setContentTitle(title.getText().toString())
+                        .setContentText(message.getText().toString())
+                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setContentIntent(pendingIntent);
 
 
-        notifiactionManager.notify(1, notification);
+
+        notifiactionManager = NotificationManagerCompat.from(this);
+
+
+
+        notifiactionManager.notify(001, mBuilder.build());
     }
 
 
